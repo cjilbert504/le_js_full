41c8ceb2c2d6ce88f686860edec9a07bacb5bcf6
Collin Jilbert <cjilbert504@gmail.com>
2020-06-01 12:39:33 -0500

    Finish the javascript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,24 @@
 function activateGallery() {
-    alert("Hello from the gallery file!");
+    let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+    let mainImage = document.querySelector("#gallery-photo img")
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.addEventListener("click", function() {
+            let newImageSrc = thumbnail.dataset.largeVersion;
+            const currentClass = "current";
+
+            mainImage.setAttribute("src", newImageSrc);
+            mainImage.setAttribute("alt", thumbnail.alt);
+
+            document.querySelector(".current").classList.remove(currentClass);
+            thumbnail.parentNode.classList.add(currentClass);
+
+            let galleryInfo = document.querySelector("#gallery-info");
+            let title = galleryInfo.querySelector(".title");
+            let description = galleryInfo.querySelector(".description");
+
+            title.innerHTML = thumbnail.dataset.title;
+            description.innerHTML = thumbnail.dataset.description;
+        });
+    });
 }
